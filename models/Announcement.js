db03041f570d6fd624e9e50f8c511bac709d13a1
GreenMan-AI <agniskreits@gmail.com>
2026-09-04 22:47:59 +0300

    liels upgreids

--- models/Announcement.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AnnouncementSchema = new mongoose.Schema({
-  textLv: { type: String, required: true, trim: true },
-  textEn: { type: String, required: true, trim: true },
-  active: { type: Boolean, default: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Announcement', AnnouncementSchema);
